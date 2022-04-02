863cd724bf3ceade62f298ca4631736748da47b8
mrican217@gmail.com <mrican217@gmail.com>
2022-04-02 02:34:01 -0400

    fixed more paths

--- controllers/index.js
+++ controllers/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 
 // const apiRoutes = require('/api');
-const homeRoutes = require('/home-routes');
-const userRoutes = require('/user-routes');
+const homeRoutes = require('./home-routes');
+const userRoutes = require('./user-routes');
 
 router.use('/', homeRoutes);
 router.use('/', userRoutes);
